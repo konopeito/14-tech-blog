8c810fa1b9348177b1532f25b46a15279575cc05
Julia <juliafritsch04@gmail.com>
2022-11-04 13:59:34 -0500

    index.js removed item

--- controllers/index.js
+++ controllers/index.js
@@ -7,7 +7,4 @@ const dashboardRoutes = require('./dashboard-routes.js');
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-router.use((req, res) => {
-    res.status(404).end();
-  });
 module.exports = router;
