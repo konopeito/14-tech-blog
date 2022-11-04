8794b9912aed47e4d752757304ec0f3c6560c2ce
Julia <juliafritsch04@gmail.com>
2022-11-03 23:48:42 -0500

    test

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -140,9 +140,8 @@ router.post('/logout', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-
-    // pass in req.body instead to only update what's passed through
+   
+    
     User.update(req.body, {
         individualHooks: true,
         where: {
